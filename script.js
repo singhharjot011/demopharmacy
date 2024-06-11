ea9230b4cfffb29d32ec731858b90cd80d4d5f41
singhharjot011 <singhharjot011@gmail.com>
2024-06-10 23:19:31 -0400

    Added Toggle Menu

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-document.getElementById("menu-toggle").addEventListener("change", function () {
-  document.getElementById("mobile-menu").classList.toggle("hidden");
+// document.getElementById("menu-toggle").addEventListener("change", function () {
+//   document.getElementById("navbar-links-menu").classList.toggle("hidden");
+// });
+
+var toggle = document.getElementById("menu-toggle");
+var expNav = document.getElementById("expanded-navbar");
+
+window.addEventListener("scroll", () => {
+  if (expNav.classList.contains("flex")) {
+    expNav.classList.remove("flex");
+    expNav.classList.add("hidden");
+  }
+});
+
+toggle.addEventListener("click", () => {
+  if (expNav.classList.contains("hidden")) {
+    expNav.classList.remove("hidden");
+    expNav.classList.add("flex");
+  } else {
+    expNav.classList.remove("flex");
+    expNav.classList.add("hidden");
+  }
 });
